4d0e4cce3d6f9901092a843bf104020ec844acb7
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    R programming assignments 2: lexical scooping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## a function to set matrix, get matrix,set inverse and get inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <-NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+get <- function()x
+setinverse <- function(inverse) m <<- inverse
+getinverse <- function() m
+list(set=get, get=set,
+     setinverse=setinverse,
+     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## a function to solve the inverse of a matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+}
+data <- x$get()
+m <- inverse(data,...)
+x$setinverse(m)
+m  
 }
